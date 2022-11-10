8a38b65331a785619844ee835998e6831ecb6d8f
Victoire <techvictorus@gmail.com>
2022-11-10 18:48:35 +0100

    correction

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
+import React from 'react';
 import '../styles/globals.css';
 import Header from "../components/Header";
 
 interface Props {
-    children: JSX.Element[] | JSX.Element
+    children?: React.ReactNode
 }
 
-const Layout = (props: Props) => {
+const Layout :React.FC<Props>= (props: Props) => {
     return (
         <html lang="en">
         <body>
